a8890afd239916704c6f0365702b9264f388b768
Teased-oChroid-orrA <132861596+Teased-oChroid-orrA@users.noreply.github.com>
2026-02-12 07:05:07 -0600

    Fix drag-and-drop functionality in BushingRightLaneCards.svelte by removing consider handler and updating items on finalize only.

---
+++ BushingRightLaneCards.svelte
@@ -0,0 +1,27 @@
+<script>
+import { onMount } from 'svelte';
+
+let items = [];
+
+function onDragEnd(event) {
+    const { draggedItem, targetItem } = event.detail;
+    // Update items only on finalize
+    const index = items.indexOf(draggedItem);
+    if (index > -1) {
+        items.splice(index, 1);
+    }
+    items.splice(items.indexOf(targetItem), 0, draggedItem);
+}
+
+onMount(() => {
+    // initialization code here
+});
+</script>
+
+<template>
+    <div class="bushing-right-lane-cards">
+        {#each items as item}
+            <div class="card" draggable on:dragend={onDragEnd}>{item}</div>
+        {/each}
+    </div>
+</template>
